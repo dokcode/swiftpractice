23d0b5202291c0f17778e6fbe1e2fcced5ac3518
David Keith <keithdrd456@gmail.com>
2026-04-04 18:05:37 -0400

    Initial Commit

---
+++ Swiftpractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftpractice
+//
+//  Created by David Keith on 4/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftpractice/SwiftpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftpracticeApp.swift
+//  Swiftpractice
+//
+//  Created by David Keith on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
